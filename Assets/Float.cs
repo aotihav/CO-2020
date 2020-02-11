36cfd80137022a5d5d7c404e031d4c2f4b6198f6
Orion Jakobovic <orionjak@gmail.com>
2020-02-11 13:02:45 -0500

    Work 2-11

---
+++ Assets/Float.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+
+    public bool animPos = true;
+    public float posAmplitude;
+    public float posSpeed;
+
+    private Vector3 origPos;
+
+    public float startAnimOffset = 0;
+
+
+    /**
+     * Awake
+     */
+    void Awake()
+    {
+        origPos = transform.position;
+        startAnimOffset = Random.Range(0f, 540f);        // so that the xyz anims are already offset from each other since the start
+    }
+
+    /**
+     * Update
+     */
+    void Update()
+    {
+        /* position */
+        if (animPos)
+        {
+            Vector3 pos;
+            pos.x = origPos.x + posAmplitude * Mathf.Sin(posSpeed * Time.time + startAnimOffset);
+            pos.y = origPos.y + posAmplitude * Mathf.Sin(posSpeed * Time.time + startAnimOffset);
+            pos.z = origPos.z + posAmplitude * Mathf.Sin(posSpeed * Time.time + startAnimOffset);
+            transform.position = pos;
+            posSpeed = Random.Range(1, 5);
+            posAmplitude = Random.Range(1, 5);
+        }
+
+    }
+}
---
+++ Assets/RandomFloat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFloat : MonoBehaviour
+{
+    Vector3 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, Random.value, z));
+    static float z = 10.49f;
+    private float _timeStartedLerping;
+    public float timeTakenDuringLerp = .2f;
+    Vector3 destinationPos;
+
+    // Transforms to act as start and end markers for the journey.
+
+    // Movement speed in units per second.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Transform transform = gameObject.GetComponent<Transform>();
+       transform.position = randomPositionOnScreen;
+       destinationPos = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, Random.value, z));
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(transform.position, destinationPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform transform = gameObject.GetComponent<Transform>();
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(transform.position, destinationPos, fractionOfJourney);
+
+    }
+}
